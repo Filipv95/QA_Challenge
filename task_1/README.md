f5e4136873d776f370c6ee8d60174b1c71574a2c
Filipv95 <filipv65@gmail.com>
2023-06-28 20:44:54 +0200

    WIP: Initial script

---
+++ task_1/README.md
@@ -0,0 +1,51 @@
+# Factorial Calculator Automation
+
+## Table of Contents
+
+-   [Description](#description)
+-   [Prerequisites](#prerequisites)
+-   [How to run test](#how-to-run-test)
+-   [Branch naming conventions](#branch-naming-convention)
+
+---
+
+## Description
+
+This repository contains automation test for Factorial Calculator web application using Playwright test automation framework written in TypeScript.
+
+---
+
+## Prerequisites
+
+**Node version 14 or above**
+
+Install Node modules, run:
+
+```
+npm i
+```
+
+Check if you already have Node instaled, run:
+
+```
+node -v
+```
+
+**Playwright version 1.33.0**
+
+Install Playwright dependencies, run:
+
+```
+npx playwright install
+```
+
+---
+
+## How to run test
+
+To run test:
+
+```
+npx playwright tsc factorial.ts
+node factorial.js
+```
---
+++ task_1/tests/factorial_script.js
@@ -0,0 +1,49 @@
+const { chromium } = require('@playwright/test');
+
+async function verifyCalculator() {
+  const url = 'https://qainterview.pythonanywhere.com/';
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  for (let number = 10; number <= 100; number++) {
+    const expectedResult = calculateFactorial(number);
+
+    try {
+      await page.goto(url);
+      await page.fill('#number', number.toString());
+      await page.click('#getFactorial');
+      await page.waitForSelector('#resultDiv');
+
+      const calculatedResult = await page.textContent('#resultDiv');
+
+      console.log(`Factorial of ${number}: Expected: ${expectedResult}, Calculated: ${calculatedResult}`);
+
+      if (calculatedResult === `The factorial of ${number} is: ${expectedResult}`) {
+        console.log(`Factorial of ${number} is correct`);
+      } else {
+        console.log(`Factorial of ${number} is incorrect`);
+      }
+    } catch (error) {
+      console.log(`Error occurred while retrieving factorial for ${number}:`, error);
+    }
+  }
+
+  await browser.close();
+}
+
+function calculateFactorial(number) {
+  let factorial = BigInt(1);
+  for (let i = 2; i <= number; i++) {
+    factorial *= BigInt(i);
+  }
+
+  if (number <= 21) {
+    return factorial.toString();
+  } else {
+    return Number(factorial.toString()).toExponential();
+  }
+}
+
+// Start the verification process
+verifyCalculator();
